a239879938e1678d7ec8b6ede748648ca2e710a2
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-06 21:07:40 +0100

    Stop using recursion
    Accepting my limits as a developer, I decided to switch the recursive function for a simple
    iterate-and-insert loop. Finally, the second star is achieved.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn read_file(file: File) -> [i32; 3] {
+fn read_file(file: File) -> Vec<i32> {
     let reader = BufReader::new(file);
     let mut total = 0;
-    let mut max_list = [0; 3];
+    let mut sorted_list = vec![0, 3];
 
     for line in  reader.lines() {
         let line = line.unwrap();
@@ -13,26 +13,17 @@ fn read_file(file: File) -> [i32; 3] {
             println!("Result: {}, Total: {}", result, total);
         } else {
             println!("No number");
-            if (total > max_list[2]) {
-                max_list = put_max(max_list, total);
+            for i in 0..sorted_list.len() {
+                if total > sorted_list[i] {
+                    sorted_list.insert(i, total);
+                    sorted_list.truncate(3); // Keep list length at 3 to reduce run time
+                    break
+                }
             }
             total = 0;
         }
     }
-    max_list
-}
-
-fn put_max(mut max_list: [i32; 3], new_val: i32) -> [i32; 3] {
-    let mut new_list = max_list.clone();
-    for (i, val) in max_list.iter_mut().enumerate() {
-        println!("Index: {}, Value: {}, New Value: {}", i, val, new_val);
-        if new_val > *val {
-            new_list[i] = new_val;
-            put_max(new_list, *val);
-            break;
-        }
-    }
-    new_list
+    sorted_list
 }
 
 fn main() {
