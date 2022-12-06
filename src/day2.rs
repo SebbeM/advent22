a0d523083abf4b793b2feebe17b3368a7a805efd
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-06 21:58:14 +0100

    Parse day2 input

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,21 @@
-pub fn run() {
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
+pub fn run() {
+    let file = File::open("input/day2").expect("File not found");
+    let reader = BufReader::new(file);
+
+    for line in  reader.lines() {
+        let line = line.unwrap();
+
+        calculate_game(line);
+    }
+}
+
+fn calculate_game(signs: String) {
+    let signs = signs.split(' ').collect::<Vec<&str>>();
+    let opponent = signs[0];
+    let me = signs[1];
+
+    println!("Opponent plays: {}, I play: {}", opponent, me);
 }
