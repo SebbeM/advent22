ac3d8e3f80e42d60f110e88fa71d570617c453b8
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-07 21:32:22 +0100

    Clean matrix initializations

--- src/day2.rs
+++ src/day2.rs
@@ -1,21 +1,22 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use phf::{phf_map};
+
+use phf::phf_map;
 
 // Matrix as [me][opponent]
-const RESULT_MATRIX: [[i32; 3]; 3] = [[3, 0, 6],
-                                    [6, 3, 0],
-                                    [0, 6, 3]];
+const RESULT_MATRIX: [[i32; 3]; 3] = [  [3, 0, 6],
+                                        [6, 3, 0],
+                                        [0, 6, 3]];
+
+// Matrix as [me][opponent]
+const STRATEGY_MATRIX: [[i32; 3]; 3] = [[2, 0, 1],
+                                        [0, 1, 2],
+                                        [1, 2, 0]];
 
 const SIGN_MAP: phf::Map<&str, i32> = phf_map![
     "A" => 0, "X" => 0,
     "B" => 1, "Y" => 1,
     "C" => 2, "Z" => 2];
-
-// Matrix as [me][opponent]
-const STRATEGY_MATRIX: [[i32; 3]; 3] = [   [2, 0, 1],
-                            [0, 1, 2],
-                            [1, 2, 0]];
 
 pub fn run() {
     let file = File::open("input/day2").expect("File not found");
