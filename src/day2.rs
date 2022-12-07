d2fff6de588ca0a23588f9d1568fbfbd8209d3fa
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-07 21:30:17 +0100

    Add sign change according to strategy

--- src/day2.rs
+++ src/day2.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use phf::{phf_map};
 
-const RESULT_MATRIX: [[i32;3];3] = [[3, 0, 6],
+// Matrix as [me][opponent]
+const RESULT_MATRIX: [[i32; 3]; 3] = [[3, 0, 6],
                                     [6, 3, 0],
                                     [0, 6, 3]];
 
@@ -10,6 +11,11 @@ const SIGN_MAP: phf::Map<&str, i32> = phf_map![
     "A" => 0, "X" => 0,
     "B" => 1, "Y" => 1,
     "C" => 2, "Z" => 2];
+
+// Matrix as [me][opponent]
+const STRATEGY_MATRIX: [[i32; 3]; 3] = [   [2, 0, 1],
+                            [0, 1, 2],
+                            [1, 2, 0]];
 
 pub fn run() {
     let file = File::open("input/day2").expect("File not found");
@@ -27,7 +33,9 @@ pub fn run() {
 fn calculate_game(signs: String) -> i32 {
     let signs = signs.split(' ').collect::<Vec<&str>>();
     let opponent = SIGN_MAP.get(signs[0]).unwrap();
-    let me = SIGN_MAP.get(signs[1]).unwrap();
+    let mut me = SIGN_MAP.get(signs[1]).unwrap();
+    // Change sign according to strategy
+    me = &STRATEGY_MATRIX[*me as usize][*opponent as usize];
 
     println!("Opponent plays: {}({}), I play: {}({})", signs[0], opponent, signs[1], me);
 
