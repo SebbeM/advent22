9ccb0a7487c3a038a00550bb82e79e2bb07c6109
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-08 11:43:51 +0100

    Solve day 4:1

--- src/day4.rs
+++ src/day4.rs
@@ -1,11 +1,38 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Split};
 
 pub fn run() {
     let file = File::open("input/day4").expect("File not found");
     let reader = BufReader::new(file);
+    let mut contained_ranges = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
+
+        if ranges_contained(line) { contained_ranges += 1 };
     }
+    println!("Number of fully contained ranges: {}", contained_ranges)
+}
+
+fn ranges_contained(line: String) -> bool {
+    let elf_pair: Vec<&str> = line.split(",").collect::<Vec<&str>>();
+
+    let range_0 = get_range(elf_pair[0]);
+    let range_1 = get_range(elf_pair[1]);
+
+    if range_0.0 >= range_1.0 && range_0.1 <= range_1.1 {
+        println!("Range {} is contained in range {}", elf_pair[0], elf_pair[1]);
+        return true
+    } else if range_0.0 <= range_1.0 && range_0.1 >= range_1.1 {
+        println!("Range {} is contained in range {}", elf_pair[1], elf_pair[0]);
+        return true
+    }
+    false
+}
+
+fn get_range(string: &str) -> (i8, i8) {
+    let mut range: Vec<&str> = string.split("-").collect();
+    let range: (i8, i8) = (range[0].parse().expect("First number could not be parsed"),
+                           range[1].parse().expect("Second number could not be parsed"));
+    range
 }
