1741214a49a3361543bd4ecb446eb0d1376a396e
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-08 09:17:08 +0100

    Solve day3, part 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,13 +1,25 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+const PRIORITIES: &str = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 pub fn run() {
     let file = File::open("input/day3").expect("File not found");
     let reader = BufReader::new(file);
+    let mut sum = 0;
 
     for line in  reader.lines() {
         let line = line.unwrap();
+        let halves = line.split_at(line.len() / 2);
 
-        println!("# of items: {}", line.len());
+        for char in halves.0.chars() {
+            if halves.1.contains(char) {
+                let priority = PRIORITIES.find(char).expect("No priority found");
+                println!("Character {} is in both halves and its priority is: {}", char, priority);
+                sum += priority;
+                break // Exactly one item type may be in both halves
+            }
+        }
     }
+    println!("Sum is: {}", sum);
 }
