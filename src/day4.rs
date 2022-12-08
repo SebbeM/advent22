da7ba91469fb43e65cbd1cac521b0a1bd3b02d12
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2022-12-08 11:59:44 +0100

    Add solution for 4:2

--- src/day4.rs
+++ src/day4.rs
@@ -5,16 +5,19 @@ pub fn run() {
     let file = File::open("input/day4").expect("File not found");
     let reader = BufReader::new(file);
     let mut contained_ranges = 0;
+    let mut overlapping_ranges = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
 
-        if ranges_contained(line) { contained_ranges += 1 };
+        if ranges_contained(&line) { contained_ranges += 1 };
+        if ranges_overlap(&line) {overlapping_ranges += 1};
     }
-    println!("Number of fully contained ranges: {}", contained_ranges)
+    println!("Number of fully contained ranges: {}", contained_ranges);
+    println!("Number of overlapping ranges: {}", overlapping_ranges);
 }
 
-fn ranges_contained(line: String) -> bool {
+fn ranges_contained(line: &String) -> bool {
     let elf_pair: Vec<&str> = line.split(",").collect::<Vec<&str>>();
 
     let range_0 = get_range(elf_pair[0]);
@@ -25,6 +28,22 @@ fn ranges_contained(line: String) -> bool {
         return true
     } else if range_0.0 <= range_1.0 && range_0.1 >= range_1.1 {
         println!("Range {} is contained in range {}", elf_pair[1], elf_pair[0]);
+        return true
+    }
+    false
+}
+
+fn ranges_overlap(line: &String) -> bool {
+    let elf_pair: Vec<&str> = line.split(",").collect::<Vec<&str>>();
+
+    let range_0 = get_range(elf_pair[0]);
+    let range_1 = get_range(elf_pair[1]);
+
+    if (range_0.0 >= range_1.0 && range_0.0 <= range_1.1) ||
+        (range_0.1 >= range_1.0 && range_0.1 <= range_1.1) ||
+        (range_1.0 >= range_0.0 && range_1.0 <= range_0.1) ||
+        (range_1.1 >= range_0.0 && range_1.1 <= range_0.1) {
+        println!("Range {} overlaps with range {}", elf_pair[0], elf_pair[1]);
         return true
     }
     false
